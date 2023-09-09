f126e0b66c80092b5c4322a8f225a5b0b8336fbd
THENUKAPRASAD <144448844+THENUKAPRASAD@users.noreply.github.com>
2023-09-09 18:51:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94772548325', 'me', true],
+  ['94772548325', 'Thenuka prasad', true], 
   [''] 
 ] //Number of owners
 
